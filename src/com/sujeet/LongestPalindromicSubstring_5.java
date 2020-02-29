3685ca82d7dbd9868a5c8a0125f8adcb108165ad
Sujeet Kumar <sujkumar@linkedin.com>
2020-02-29 14:05:05 -0800

    Initial commit

---
+++ src/com/sujeet/LongestPalindromicSubstring_5.java
@@ -0,0 +1,42 @@
+package com.sujeet;
+
+public class LongestPalindromicSubstring_5 {
+    public String longestPalindrome(String s) {
+        String res = "";
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        int maxLen = 0;
+        int bestStart = 0;
+        int bestEnd = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int st = i;
+            int end = i;
+            while (st >= 0 && end < s.length() && s.charAt(st) == s.charAt(end)) {
+                int curLen = end - st + 1;
+                if (maxLen < curLen) {
+                    maxLen = curLen;
+                    bestStart = st;
+                    bestEnd = end;
+                }
+                st--;
+                end++;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            int st = i;
+            int end = i + 1;
+            while (st >= 0 && end < s.length() && s.charAt(st) == s.charAt(end)) {
+                int curLen = end - st + 1;
+                if (maxLen < curLen) {
+                    maxLen = curLen;
+                    bestStart = st;
+                    bestEnd = end;
+                }
+                st--;
+                end++;
+            }
+        }
+        return s.substring(bestStart, bestEnd + 1);
+    }
+}
---
+++ src/com/sujeet/Main.java
@@ -0,0 +1,8 @@
+package com.sujeet;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
